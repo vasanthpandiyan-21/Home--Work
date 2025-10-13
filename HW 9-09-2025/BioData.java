1d79a7f08cae23bae22f4f10992105be243f5e16
vasanthpandiyan-21 <vasanthpandiyan21@gmail.com>
2025-10-13 21:00:56 +0530

    Add files via upload

---
+++ HW 9-09-2025/BioData.java
@@ -0,0 +1,25 @@
+class BioData
+{
+   public static void main(String[]args)
+   {
+	   String name ="Vasantha pandiyan";
+	   String fatherName ="	Nagarajan";
+	   String motherName ="Nagarathinam";
+	   char  initial ='N';
+	   char  initial1 = 'A';
+	   char  initial2 ='N';
+	   byte age = 23;
+	   int salary =200000;
+	   byte  date =21;
+	   byte month =3;
+	   short year =2002;
+	   String place = "Karumathur";
+	   System.out.println("My name is "+name+"."+initial);
+	   System.out.println("My father names is"+fatherName+"."+initial1);
+	   System.out.println("My mother names is "+motherName+"."+initial2);
+	   System.out.println("MY age is "+age );
+	   System.out.println("My date of brith"+date+"/"+month+"/"+year);
+	   System.out.println("My area is "+place);
+	   System.out.println("My month income  is" +salary);
+    }
+}   
