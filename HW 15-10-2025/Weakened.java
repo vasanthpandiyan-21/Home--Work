7902ee5ad2d0379b19c581cdf6bd14a95daa7b36
vasanthpandiyan-21 <vasanthpandiyan21@gmail.com>
2025-10-15 22:03:11 +0530

    Add files via upload

---
+++ HW 15-10-2025/Weakened.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Weakened
+{
+  public static void main(String[]arhgs)
+  {
+	 Scanner virat= new Scanner(System.in);	
+     int num = virat .nextInt();
+     switch (num)
+	 {
+	 case 1:
+	 System.out.println("sollu nanba");
+	 break;
+	 
+	 case 2:
+	 System.out.println("Hello sir");
+	 break;
+	 
+	 case 3:
+	 System.out.println("Bollo bhaiya");
+	 break;
+	 default:
+	 System.out.println(" sorry you enter wrong number ");
+	 
+	 }
+	 
+}
+}
+
